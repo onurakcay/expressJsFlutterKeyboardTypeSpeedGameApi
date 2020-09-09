f3b2bca12fdec8e6139627070ce8670dbe2adaa7
Onur AKÇAY <onurakcay.tr@gmail.com>
2020-09-09 12:59:10 +0300

    update score

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,22 @@ const users = [];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(users.sort((user1,user2) =>
-    Number(user1.score) <= Number(user2.score)).slice(0, 10));
-  });
-
+  res.send(users.sort((user1,user2) => {
+    if (user1.score < user2.score) {
+      return 1;
+    }
+    if (user1.score > user2.score) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  }).slice(0,10)
+  )
+});
 
 router.post('/score', function(req, res, next) {
   const {score,userName} = req.body;
-  users.push({score, userName});
+  users.push({score: Number(score), userName});
   res.send({success: true});
 });
 
